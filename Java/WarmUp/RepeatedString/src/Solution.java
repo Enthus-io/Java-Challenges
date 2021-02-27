41d6dce8a2c8ce9a0c6360684a694a8143d53700
Enthus-io <lfojedaguzman@gmail.com>
2021-02-27 14:12:12 -0600

    Added solution to Repeated String

--- Java/WarmUp/RepeatedString/src/Solution.java
+++ Java/WarmUp/RepeatedString/src/Solution.java
@@ -5,9 +5,24 @@ public class Solution {
 
     // Complete the repeatedString function below.
     static long repeatedString(String s, long n) {
+        long x = (long) (n / s.length());
+        long remainder = (long) (n % s.length());
+        long numberOfAs = 0;
+        long tailAs = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == 'a') {
+                numberOfAs++;
+
+                if (i < remainder) {
+                    tailAs++;
+                }
+            }
+        }
 
 
-        return 0;
+        return x * numberOfAs + tailAs;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -20,6 +35,8 @@ public class Solution {
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         long result = repeatedString(s, n);
+
+        System.out.println(result);
 
         scanner.close();
     }
