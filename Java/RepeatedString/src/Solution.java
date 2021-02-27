604f332a2e89ff291307615b73c682f617115a22
Enthus-io <lfojedaguzman@gmail.com>
2021-02-27 13:10:51 -0600

    Initial Repeated String challenge

---
+++ Java/RepeatedString/src/Solution.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+
+
+        return 0;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        String s = scanner.nextLine();
+
+        long n = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = repeatedString(s, n);
+
+        scanner.close();
+    }
+}
