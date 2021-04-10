db938dc7e34cfca6d1bcf1a29ae088c3bf0be70d
Enthus-io <lfojedaguzman@gmail.com>
2021-04-10 12:12:35 -0500

    Added my solution to minimum swaps 2

--- Java/Minimum Swaps 2/src/Solution.java
+++ Java/Minimum Swaps 2/src/Solution.java
@@ -10,8 +10,32 @@ public class Solution {
 
     // Complete the minimumSwaps function below.
     static int minimumSwaps(int[] arr) {
+        int swaps = 0;
 
-        return 1;
+        for (int i = 0; i < arr.length - 1; i++) {
+            int out = 0;
+
+            for (int j = i; j + 1 < arr.length; j++) {
+                if(arr[j + 1] == j + 1) {
+                    int t = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = t;
+                    swaps++;
+                }
+                }
+
+            int a = arr[arr.length - 1];
+            int b =  arr.length - 1;
+            if(arr[arr.length - 1] != arr.length) {
+                int t = arr[arr[arr.length - 1] - 1];
+                arr[arr[arr.length - 1] - 1] = arr[arr.length - 1];
+                arr[arr.length - 1] = t;
+                swaps++;
+            }
+        }
+
+
+        return swaps;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -32,6 +56,7 @@ public class Solution {
         }
 
         int res = minimumSwaps(arr);
+        System.out.println(res);
 
         scanner.close();
     }
