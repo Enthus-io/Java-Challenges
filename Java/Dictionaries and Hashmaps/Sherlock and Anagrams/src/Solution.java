c39c04fed1c369aeede16da927f5bcabffc4aac7
Enthus-io <lfojedaguzman@gmail.com>
2021-04-13 16:06:00 -0500

    Added problem Sherlock and anagrams

---
+++ Java/Dictionaries and Hashmaps/Sherlock and Anagrams/src/Solution.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the sherlockAndAnagrams function below.
+    static int sherlockAndAnagrams(String s) {
+
+        return 1;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File mFile = new File(System.getProperty("user.dir") + "\\src\\" + "case_00.txt");
+        Scanner scanner = new Scanner(mFile);
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = sherlockAndAnagrams(s);
+            System.out.println(result);
+        }
+
+        scanner.close();
+    }
+}
