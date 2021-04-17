ae34d0c440663c8a2feeccbbc2ff6b81efc805a7
Enthus-io <lfojedaguzman@gmail.com>
2021-04-17 13:46:56 -0500

    Added my solution to Sherlock and Anagrams

--- Java/Dictionaries and Hashmaps/Sherlock and Anagrams/src/Solution.java
+++ Java/Dictionaries and Hashmaps/Sherlock and Anagrams/src/Solution.java
@@ -12,6 +12,26 @@ public class Solution {
     static int sherlockAndAnagrams(String s) {
         int count = 0;
 
+        for (int i = 1; i < s.length(); i++) {
+            Map<String, Integer> list = new HashMap<>();
+            String t = s;
+
+            for (int j = 0; j + i <= s.length(); j++) {
+                //Sorting the string to reduce time complexity
+                char tempArray[] = t.substring(j, j + i).toCharArray();
+                Arrays.sort(tempArray);
+                String c = new String(tempArray);
+
+                //Keeping track of the number_of_identical_anagrams
+                if (list.get(c) == null)
+                    list.put(c, 1);
+                else
+                    list.put(c, list.get(c) + 1);
+
+                //Adding factorial(number_of_identical_anagrams - 1)
+                count += list.get(c) - 1;
+            }
+        }
 
         return count;
     }
