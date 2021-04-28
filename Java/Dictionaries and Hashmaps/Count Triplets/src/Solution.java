7509395bc411b6a70eca3d346ee479baef738d28
Enthus-io <lfojedaguzman@gmail.com>
2021-04-27 19:00:48 -0500

    My solution to Count Triplets

--- Java/Dictionaries and Hashmaps/Count Triplets/src/Solution.java
+++ Java/Dictionaries and Hashmaps/Count Triplets/src/Solution.java
@@ -15,14 +15,33 @@ public class Solution {
 
     // Complete the countTriplets function below.
     static long countTriplets(List<Long> arr, long r) {
+        // Time complexity is O(n)
         long count = 0;
+        Map<Long, Integer> hashList = new HashMap<Long, Integer>();
 
+        // Identify multiple entries
+        for (long i : arr) {
+            if(hashList.get(i) == null)
+                hashList.put(i, 1);
+            else
+                hashList.put(i, hashList.get(i) + 1);
+        }
+
+        // Count geometric progressions for every ith element in arr
+        for (long i : arr) {
+            long j = i * r;
+            long k = i * r * r;
+
+            if(hashList.containsKey(j) && hashList.containsKey(k)) {
+                count += Math.pow(2, hashList.get(j) - 1) * Math.pow(2, hashList.get(k) - 1);
+            }
+        }
 
         return count;
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\src\\" + "case_00.txt"));
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\src\\" + "case_02.txt"));
 
         String[] nr = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
