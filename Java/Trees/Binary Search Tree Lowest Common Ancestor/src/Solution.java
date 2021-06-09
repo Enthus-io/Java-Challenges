43ffec64eead650de335d7d701e040a2d2256a00
Enthus-io <lfojedaguzman@gmail.com>
2021-06-09 09:44:45 -0500

    Setup for Binary Search Tree Lowest Common Ancestor

---
+++ Java/Trees/Binary Search Tree Lowest Common Ancestor/src/Solution.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+    /*
+    class Node
+        int data;
+        Node left;
+        Node right;
+    */
+    public static Node lca(Node root, int v1, int v2) {
+        // Write your code here.
+        return root;
+    }
+
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File(System.getProperty("user.dir") + "\\src\\" + "case_02.txt"));
+        int t = scan.nextInt();
+        Node root = null;
+        while (t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        int v1 = scan.nextInt();
+        int v2 = scan.nextInt();
+        scan.close();
+        Node ans = lca(root, v1, v2);
+        System.out.println(ans.data);
+    }
+}
