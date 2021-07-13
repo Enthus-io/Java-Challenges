ee149c537682e0b38751bf7d30aa9349359745d3
Enthus-io <lfojedaguzman@gmail.com>
2021-07-13 12:45:32 -0500

    Working on Count Triplets

--- Java/Dictionaries and Hashmaps/Count Triplets/src/Solution.java
+++ Java/Dictionaries and Hashmaps/Count Triplets/src/Solution.java
@@ -1,11 +1,5 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
 import java.util.stream.*;
 
 import static java.util.stream.Collectors.joining;
@@ -13,43 +7,54 @@ import static java.util.stream.Collectors.toList;
 
 public class Solution {
 
-    // Complete the countTriplets function below.
+    //     Complete the countTriplets function below.
     static long countTriplets(List<Long> arr, long r) {
         // Time complexity is O(n)
         long count = 0;
-        Map<Long, Integer> hashList = new HashMap<>();
-
-        for (int i = arr.size() - 1; i >= 0; i--) {
-            long ith = arr.get(i);
-            long jth = ith * r;
-            long kth = jth * r;
+        Map<Long, Long> ij = new HashMap<>();
+        Map<Long, Long> jk = new HashMap<>();
 
 
-            if (hashList.get(ith) == null)
+        for (int index = arr.size() - 1; index >= 0; index--) {
+            long i = arr.get(index);
+            long j = i * r;
+            long k = j * r;
 
-                hashList.put(ith, 1);
-            else
-                hashList.put(ith, hashList.get(ith) + 1);
 
-            if(hashList.containsKey(ith / r)){
-                hashList.remove(ith / r);
+            if(ij.get(i) == null){
+                ij.put(i, 1L);
+
             }
-            if(hashList.containsKey(ith / r / r)){
-                hashList.remove(ith / r / r);
+            else{
+                ij.put(i, ij.get(i) + 1);
             }
 
-            if(hashList.containsKey(ith * r) && hashList.containsKey(ith * r * r)){
-                count += hashList.get(ith * r) * hashList.get(ith * r * r);
+            if(ij.get(j) != null){
+                System.out.println("found i->j");
+
+                if(jk.get(j) == null){
+                    jk.put(j, 1L);
+
+                }
+                else{
+                    jk.put(j, jk.get(j) + 1);
+                }
+
+
+
+                if(jk.get(k) != null){
+                    System.out.println("found i->j->k");
+                    count = count + jk.get(k) * ij.get(k);
+                }
+
             }
-
-
         }
 
         return count;
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\src\\" + "case_14.txt"));
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\src\\" + "case_02.txt"));
 
         String[] nr = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
