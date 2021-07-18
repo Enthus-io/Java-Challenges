1ad0024e4d1b7d81634c9c6eb62405831d28db15
Enthus-io <29191182+Enthus-io@users.noreply.github.com>
2021-07-18 17:22:49 -0500

    Completed Sorting Bubble Sort

---
+++ Sorting/Sorting Bubble Sort/src/Solution.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'countSwaps' function below.
+     *
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static void countSwaps(List<Integer> a) {
+        // Write your code here
+        Integer n = a.size();
+        Integer swaps = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (a.get(j) > a.get(j + 1)) {
+                    Collections.swap(a, j, j + 1);
+                    swaps++;
+                }
+            }
+        }
+
+        System.out.println("Array is sorted in " + swaps + " swaps.");
+        System.out.println("First Element: " + a.get(0));
+        System.out.println("Last Element: " + a.get(n - 1));
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\src\\" + "case_02.txt"));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Result.countSwaps(a);
+
+        bufferedReader.close();
+    }
+}
